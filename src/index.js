e1e4d1d6cd1623da8f28f69d041b3064416aa935
Shevchenko Viacheslav <shevchenko@MacBook-Pro-Shevchenko.local>
2018-02-26 21:17:06 +0200

    Create login component, home component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    </div>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
